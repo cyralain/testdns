542ee825871df91223a971ee48e56a73b70bdd65
Alain Cyr <3778737+cyralain@users.noreply.github.com>
2024-05-21 22:50:10 -0400

    Update app.vue

--- app.vue
+++ app.vue
@@ -3,8 +3,8 @@
     Branche : dev-34<br/>
     DNS : <br/>
     - Type: ns<br/>
-    - Name: dev-33<br/>
-    - Data: dns1.p02.nsone.net.<br/>
+    - Name: dev-34<br/>
+    - Data: dns1.p05.nsone.net.<br/>
     - TTL: 1 hour<br/>
   </div>
 </template>
